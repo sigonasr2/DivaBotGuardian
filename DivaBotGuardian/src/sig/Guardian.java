e9e3a0bf0d027205e1476dc8369b754cb0f0a5bd
sigonasr2 <sigonasr2@gmail.com>
2020-09-25 18:17:29 +0900

    Create DivaBotGuardian to monitor stream input.

---
+++ DivaBotGuardian/src/sig/Guardian.java
@@ -0,0 +1,47 @@
+package sig;
+import java.io.File;
+import java.io.IOException;
+
+import sig.utils.FileUtils;
+
+public class Guardian {
+	public static int USERID = -1;
+	public static long streamLastModified = -1;
+	public static STAGE currentStage = STAGE.STARTING;
+	
+	enum STAGE{
+		STARTING,
+		RUNNING,
+		CLEANUP;
+	}
+	
+	public static void main(String[] args) throws InterruptedException, IOException {
+		File f = new File("streams","output"+USERID+".png");
+		File tempf = new File("streams","tempoutput"+USERID+".png");
+		switch (currentStage) {
+			case STARTING:{
+				USERID = Integer.parseInt(args[0]);
+				while (currentStage==STAGE.STARTING) {
+					if (f.exists()) {
+						streamLastModified = f.lastModified();
+						currentStage=STAGE.RUNNING;
+					}
+					Thread.sleep(100);
+				}
+			}break;
+			case RUNNING:{
+				while (currentStage==STAGE.RUNNING) {
+					FileUtils.copyFile(f, tempf);
+					if (System.currentTimeMillis()>=streamLastModified+5000) {
+						currentStage=STAGE.CLEANUP;
+						System.out.println("Stream is no longer being updated! Shutting down!");
+					}
+					Thread.sleep(100);
+				}
+			}break;
+			case CLEANUP:{
+				f.delete();
+			}break;
+		}
+	}
+}
